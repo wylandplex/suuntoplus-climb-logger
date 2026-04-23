2d09ad312be1ff3a5f4a29cc968eae4b4499d391
wylandplex <34101353+wylandplex@users.noreply.github.com>
2026-04-23 12:52:04 +0200

    Sync from workspace

--- ext10.js
+++ ext10.js
@@ -17,6 +17,7 @@ ats.sendPct=Math.round(ats.totalSends*100/ats.totalRoutes);
 var sv=localStorage.getObject("stats")||{};
 sv.totalRoutes=ats.totalRoutes;sv.totalSends=ats.totalSends;sv.sendPct=ats.sendPct;sv.sessions=ats.sessions;
 sv.totalHeight=(sv.totalHeight||0)+(h&&h>0?h:0);
+var bk=sv.buckets||{};var bKey=lgs+"_"+lgi;var bb=bk[bKey]||{a:0,s:0};bb.a++;if(isSend)bb.s++;bk[bKey]=bb;sv.buckets=bk;
 if(cm>0){var ap=ps[lgs+"_"+cm];if(ap){sv.activeGrade=lgs*100+lgi;sv.activeTries=ap.attempts;sv.activeSends=ap.sends;sv.activeBest=ap.bestTime}}
 if(firstSend){if(projTries>(sv.mostTriesProject||0)){sv.mostTriesProject=projTries;sv.mostTriesGrade=projEnc;}if(projSes>(sv.longestProjectSes||0)){sv.longestProjectSes=projSes;sv.longestProjectGrade=projEnc;}}
 if(isSend){var LENS=[41,24,29,11,11,12,14,30];function nm(e){return e<0?-1:(e%100)/(LENS[Math.floor(e/100)]-1)}var enc2=lgs*100+lgi;if(nm(enc2)>nm(sv.lastSessionGrade||-1))sv.lastSessionGrade=enc2;if(nm(enc2)>nm(sv.peakGrade||-1)){sv.peakGrade=enc2;sv.peakSession=ats.sessions||0}}
